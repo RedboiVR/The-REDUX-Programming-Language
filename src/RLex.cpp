52720f60e3628024fcd0f06b02dee1198bd79624
RedboiVR <binarywaterfall@gmail.com>
2026-09-08 19:03:13 -0700

    workin on it

--- src/RLex.cpp
+++ src/RLex.cpp
@@ -1,12 +1,16 @@
 #pragma once
 #include "headers/RLex.hpp"
 
-// literally the only definition.
-// man i love C++
 void Tokens::AddToken(TokenType type, std::string value){
     TokenList.PushBack({type, value});
 }
 
+
 void Tokens::Lex(std::string ln){
-    
+    std::string StrChunk; // the value that'll be passed into a token
+    TokenType decidedtype; // A decided type that'll be passed into a token
+    char RecentCharacter; // the character that'll join into the StrChunk or listen for signals to change the type on the ready
+    for(){
+        
+    }
 }

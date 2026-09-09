374f3fc76f9cf0dc2b34d3231ce0b4f7a7795e5b
RedboiVR <binarywaterfall@gmail.com>
2026-09-08 19:20:56 -0700

    Update RLex.cpp to fledge out the lex function

--- src/RLex.cpp
+++ src/RLex.cpp
@@ -5,12 +5,29 @@ void Tokens::AddToken(TokenType type, std::string value){
     TokenList.PushBack({type, value});
 }
 
-
+/*
+typedef enum {
+    Keyword,
+    Declaration,
+    Type,
+    Value
+} TokenType;
+*/
 void Tokens::Lex(std::string ln){
     std::string StrChunk; // the value that'll be passed into a token
     TokenType decidedtype; // A decided type that'll be passed into a token
     char RecentCharacter; // the character that'll join into the StrChunk or listen for signals to change the type on the ready
-    for(){
-        
+    for(size_t i=0; i<ln.size(); i++){
+        RecentCharacter = ln[i];
+        switch(RecentCharacter){
+            default:
+                StrChunk += RecentCharacter;
+                break;
+            case ':':
+                // Flush Token
+                decidedtype = Type;
+                break;
+            
+        }
     }
 }

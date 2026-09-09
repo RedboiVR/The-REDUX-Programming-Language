62e18aa889f63d66d2fc55f3b45b2547233f4561
RedboiVR <binarywaterfall@gmail.com>
2026-09-08 18:35:58 -0700

    Define RLex.hpp

--- src/RLex.cpp
+++ src/RLex.cpp
@@ -1,1 +1,8 @@
-// nothing yet :(
+#pragma once
+#include "headers/RLex.hpp"
+
+// literally the only definition.
+// man i love C++
+void Tokens::AddToken(TokenType type, std::string value){
+    TokenList.PushBack({type, value});
+}

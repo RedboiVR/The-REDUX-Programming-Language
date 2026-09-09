5d2c7121cac915dc6d7b46ffc09842a678b53c3d
RedboiVR <binarywaterfall@gmail.com>
2026-09-09 11:19:32 -0700

    Add AssembleAST function to Parser class
    Implement AssembleAST function in Parser class.

---
+++ src/RPrse.cpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "RPrse.hpp"
+#include "RKey.hpp"
+
+void Parser::AssembleAST(std::vector<Token> Tokens){
+    Tokens.clear();
+    ParentNodes* CurrentNode = nullptr;
+
+    for (Token : Token Tokens){
+        ;
+    }
+}
